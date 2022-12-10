fd5e889266c6325cfc20075c86f0b77af649b116
Pedro Mendoza <pedro.mendoza2482@gmail.com>
2022-12-10 17:13:45 -0600

    cambio en el index html para generar paquete a producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  url: 'http://localhost:5000/api/',
+  url: 'http://infinityhn.net:5001/api/',
   tpUrl: 'https://preprod-api.todopago.hn/pay',
   tpUser: '0501-9012-46995512',
   tpPassword: '123456'
