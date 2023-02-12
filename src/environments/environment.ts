fba3dd76b084045db4d2b5a2e5150b0737a73cf0
Pedro Mendoza <pmendoza@caracolknits.com>
2023-02-12 13:11:32 -0600

    agregando positiveSSL a la app

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@
 
 export const environment = {
   production: false,
-  //url: 'http://infinityhn.net:5001/api/',
-  url: 'http://localhost:5000/api/',
+  url: 'http://infinityhn.net:5001/api/',
+  //url: 'http://localhost:5000/api/',
   tpUrl: 'https://preprod-api.todopago.hn/pay',
   tpUser: '0501-9012-46995512',
   tpPassword: '123456'
