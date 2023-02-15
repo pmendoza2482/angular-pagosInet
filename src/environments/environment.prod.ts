8b37e5add4124e8d554509a432416739a5814915
Pedro Mendoza <pedro.mendoza2482@gmail.com>
2023-02-14 21:00:36 -0600

    revisando onditas

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  //url: 'http://infinityhn.net:5001/api/',
-  url: 'http://localhost:5000/api/',
+  url: 'http://infinityhn.net:5001/api/',
+  //url: 'http://localhost:5000/api/',
   tpUrl: 'https://preprod-api.todopago.hn/pay',
   tpUser: '0501-9012-46995512',
   tpPassword: '123456'
